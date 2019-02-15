81e4f2dfd862cbbdd152770d3ac6c307ecc9730b
rchu6120 <rchu6120@bths.edu>
2019-02-15 08:30:49 -0500

    dhiaso

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,75 @@
 /**
- * Created by Teacher on 1/7/2019.
+ * Card.java
+ *
+ * <code>Card</code> represents a playing card.
  */
 public class Card {
+
+    /**
+     * String value that holds the suit of the card
+     */
+    private String suit;
+
+    /**
+     * String value that holds the rank of the card
+     */
+    private String rank;
+
+    /**
+     * int value that holds the point value.
+     */
+    private int pointValue;
+
+
+    /**
+     * Creates a new <code>Card</code> instance.
+     *
+     * @param cardRank  a <code>String</code> value
+     *                  containing the rank of the card
+     * @param cardSuit  a <code>String</code> value
+     *                  containing the suit of the card
+     * @param cardPointValue an <code>int</code> value
+     *                  containing the point value of the card
+     */
+    public Card(String cardRank, String cardSuit, int cardPointValue) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        this.rank = cardRank;
+        this.suit = cardSuit;
+        this.pointValue = cardPointValue;
+    }
+
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", rank='" + rank + '\'' +
+                ", pointValue=" + pointValue +
+                '}';
+    }
 }
+
